fcbc3dbdfdaa41a2c4c58d315a1e95a41b16a55b
swethatech <74248723+swethatech@users.noreply.github.com>
2021-01-09 11:58:59 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import task1
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
